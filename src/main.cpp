702d4641e0932aaaf941011e3fdbc523ca7ddf61
Hassan Qasemi <hassanak813@outlook.com>
2026-07-09 17:04:50 +0330

    Working version

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "cli.hpp"
 #include "config.hpp"
+#include "project_generator.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -12,13 +13,23 @@ int main(int argc, char *argv[])
 
     if (cli.project_type() == ProjectType::C)
     {
-        std::println("C");
-        std::println("{}", name);
+        auto result = C_Generator::scaffold_project(name, conf.c_std());
+        if (!result)
+        {
+            std::println(stderr, "Error: {}: {}", to_string(result.error()),
+                         name);
+            return 1;
+        }
     }
-    else if (cli.project_type() == ProjectType::CPP)
+    else if (cli.project_type() == ProjectType::CXX)
     {
-        std::println("CPP");
-        std::println("{}", name);
+        auto result = CXX_Generator::scaffold_project(name, conf.cxx_std());
+        if (!result)
+        {
+            std::println(stderr, "Error: {}: {}", to_string(result.error()),
+                         name);
+            return 1;
+        }
     }
 
     return 0;
